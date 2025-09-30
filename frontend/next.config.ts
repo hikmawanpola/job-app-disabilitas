fa3a583be26b886b2672d0be3527627139eddc08
hikmawanpola <hikmawan02pp@gmail.com>
2025-09-30 19:12:16 +0700

    bismillah

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: { typedRoutes: true },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
 };
-
 export default nextConfig;
